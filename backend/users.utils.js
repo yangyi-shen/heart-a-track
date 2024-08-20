8fb748eb59b5068ce569a0eae6680f125b6be30e
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-20 13:30:29 +0800

    feat: write register & login functions

--- backend/users.utils.js
+++ backend/users.utils.js
@@ -5,20 +5,38 @@ const connectionString = process.env.DATABASE_URI
 
 async function registerUser(username, password) {
     const sql = postgres(connectionString)
+    const exists = await checkUser(username, password)
 
-    const response = await sql`
-        INSERT INTO users (username, password) VALUES (${username}, ${password})
-    `
+    if (exists) {
+        throw new Error('ERROR: User already exists!')
+    } else {
+        await sql`
+            INSERT INTO users (username, password) VALUES (${username}, ${password})
+        `
+    }
 
     await sql.end();
 }
 
-async function loginuser(username, password) {
+async function loginUser(username, password) {
+    const sql = postgres(connectionString)
+    const exists = await checkUser(username, password)
+
+    if (!exists) {
+        throw new Error('ERROR: User does not exist!')
+    }
+    
+    await sql.end();
+}
+
+async function checkUser(username, password) {
     const sql = postgres(connectionString)
 
     const response = await sql`
-        SELECT * FROM users WHERE (username = ${username} AND password = ${password})
+        SELECT EXISTS(SELECT 1 FROM users WHERE username = ${username} AND password = ${password})
     `
-    
+    const exists = response[0].exists;
+
     await sql.end();
+    return exists;
 }
