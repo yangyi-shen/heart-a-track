8807a5ff7cbef3cbd4dca571c37ac12f24302664
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-21 10:41:15 +0800

    feat: init server and outline apis

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,43 @@
-import postgres from "postgres"
-import 'dotenv/config';
+import express from 'express'
 
-const connectionString = process.env.DATABASE_URI
+const app = express()
+const PORT = 6900
 
-const sql = postgres(connectionString)
-await sql.end();
+// AUTHENTICATION APIs
+app.post('/auth/register', (req, res) => {
+    // params: 
+    // - username
+    // - password
+
+    // register new user
+})
+
+app.post('/auth/login', (req, res) => {
+    // params: 
+    // - username
+    // - password
+
+    // validate provided credentials
+})
+
+// DATA MANAGEMENT APIs
+app.put('/data/write', (req, res) => {
+    // params: 
+    // - userId
+    // - bp
+    // - hr
+
+    // create new data entry
+})
+
+app.get('/data/get', (req, res) => {
+    // params:
+    // - userId
+    // - range
+
+    // return data for specified user and range
+})
+
+app.listen(PORT, () => {
+    console.log('server running on port 6900')
+})
