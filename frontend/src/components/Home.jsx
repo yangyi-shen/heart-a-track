6a686c2e705b21be95225ab55babc2750f964758
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-22 11:50:07 +0800

    feat: change homepage for easy navigation

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,11 @@ export default function Main() {
     return (
         <main>
             <h1>This is the home page</h1>
-            <p>Check out the <Link to="/about">about</Link> page or the <Link to="/login">login</Link> page</p>
+            <ul>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/record">Record</Link></li>
+            </ul>
         </main>
     )
 }
